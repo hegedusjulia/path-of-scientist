868b141db19e31d3ce0df94766474ddf71521022
Hegedűs Júlia <hegedus.julia.erzsebet@gmail.com>
2023-05-05 20:53:47 +0200

    Initial commit
    Initial commit

---
+++ src/main/java/game/PathOfScientist.java
@@ -0,0 +1,43 @@
+package game;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class PathOfScientist {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PathOfScientist window = new PathOfScientist();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PathOfScientist() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
